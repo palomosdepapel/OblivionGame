f40ed7938df05e6068619aedb65e0b94d1db73f5
Juan Carlos Huertas <jhuertas2016@gmail.com>
2023-01-05 22:36:01 -0500

    Desafío primera implementación de mecánica

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -4,26 +4,41 @@ using UnityEngine;
 
 public class Character : MonoBehaviour
 {
-    public int age; //edad del jugador
-    public float health; // vida del jugador
-    public float damage; // daño del jugador
-    public float defense;
-    public bool useRaytracing;
-    public double earthRotation;
-    public Vector2 gridPosition;
-    public Vector3 myPosition;
-    public Vector4 viewPosition;
     [SerializeField] private string characterName; // con atributo SerializeField para guardar
+    public float characterHealth = 100; // vida del jugador
+    public float characterSpeed = 1;
+    //public float damage; // daño del jugador
+    //public int age; //edad del jugador
+    //public float defense;
+    //public bool useRaytracing;
+    //public double earthRotation;
+    //public Vector2 gridPosition;
+    //public Vector3 myPosition;
+    //public Vector4 viewPosition;
+    
 
     // Start is called before the first frame update
     void Start()
     {
         characterName = "Jack Harper Tech 49";
+        characterSpeed = 2.2f;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    // daño
+    public void characterDamage()
+    {
+        characterHealth = characterHealth - 5;
+    }
+
+    // cura
+    public void characterHeal()
+    {
+        characterHealth = characterHealth + 10;
     }
 }
--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -5,28 +5,61 @@ using UnityEngine;
 public class EnemyScript : MonoBehaviour
 {
     [SerializeField] private string enemyName;
-    [SerializeField] private Vector3 movement;
-    [SerializeField] private Vector3 size;
-    public float enemySpeed;
-    public float damage;
-    [SerializeField] private Character character;
+    public float enemyHealth = 100; // vida del jugador
+    public float enemySpeed = 1;
+    [SerializeField] private Vector3 direction;
+    //[SerializeField] private Vector3 size;
+    //public float enemySpeed;
+    //public float damage;
+    //[SerializeField] private Character character;
 
     // Start is called before the first frame update
     void Start()
     {
-        damage = 2;
+        //damage = 2;
         enemyName = "Drone 166";
-        enemySpeed = 1;
+        enemySpeed = 0.1f;
         // vida CHAR = vida CHAR - daño ES - defensa CHAR
         // character.health = character.health - damage - character.defense;
-        character.health -= damage - character.defense;
+        // character.health -= damage - character.defense;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position += movement;
-        transform.localScale += size;
-        transform.position += enemySpeed * Time.deltaTime * transform.forward;
+        Movement(0.1f, new Vector3(0, 0.01f, 0));
+        //transform.position += movement;
+        //transform.localScale += size;
+        //transform.position += enemySpeed * Time.deltaTime * transform.forward;
+
     }
+
+    // daño
+    public void enemyDamage()
+    {
+        enemyHealth = enemyHealth -2;
+    }
+
+    // cura
+    public void enemyHeal()
+    {
+        enemyHealth = enemyHealth + 5;
+    }
+
+    
+    public void increaseEnemySpeed()
+    {
+        enemySpeed += 1;
+    }
+
+    public void decreaseEnemySpeed()
+    {
+        enemySpeed -= 1;
+    }
+
+    public void Movement(float speedEnemy, Vector3 direction)
+    {
+        transform.position += transform.position * enemySpeed * Time.deltaTime;
+    }
+
 }
