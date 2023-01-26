81866bd09ccd3ce0eeb3bf31a33dcfa23b810ef4
Juan Carlos Huertas <jhuertas2016@gmail.com>
2023-01-25 21:47:58 -0500

    Desafio Cámara

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -7,15 +7,12 @@ public class Character : MonoBehaviour
     [SerializeField] private string characterName; // con atributo SerializeField para guardar
     [SerializeField] private float characterHealth = 100; // vida del jugador
     [SerializeField] private float characterSpeed = 1;
-    //public float damage; // daño del jugador
-    //public int age; //edad del jugador
-    //public float defense;
-    //public bool useRaytracing;
-    //public double earthRotation;
-    //public Vector2 gridPosition;
-    //public Vector3 myPosition;
-    //public Vector4 viewPosition;
-    
+    [SerializeField] private float speed = 2;
+    [SerializeField] private float maxSpeed = 100;
+    [SerializeField] private float minSpeed = 1;
+    [SerializeField] private float rootSpeed1 = 50;
+    [SerializeField] private float rootSpeed2 = 50;
+
 
     // Start is called before the first frame update
     void Start()
@@ -27,7 +24,39 @@ public class Character : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position += transform.forward * speed * Time.deltaTime;
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (speed <= maxSpeed)
+            {
+                speed += 1;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            if (speed >= minSpeed)
+            {
+                speed -= 1;
+            }
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow) == Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward * rootSpeed2 * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow) == Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(Vector3.back * rootSpeed2 * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.UpArrow) == Input.GetKey(KeyCode.S))
+        {
+            transform.Rotate(Vector3.left * rootSpeed1 * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.DownArrow) == Input.GetKey(KeyCode.W))
+        {
+            transform.Rotate(Vector3.right * rootSpeed1 * Time.deltaTime);
+        }
     }
 
     // daño
