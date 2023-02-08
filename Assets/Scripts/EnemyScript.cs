e9f6041713b5f06f222aa1389ba3ddb3eb7ed4f1
Juan Carlos Huertas <jhuertas2016@gmail.com>
2023-02-07 22:21:01 -0500

    Destroy enemy with bullets

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -6,71 +6,41 @@ public class EnemyScript : MonoBehaviour
 {
     [SerializeField] private string enemyName;
     [SerializeField] private float enemyHealth = 100; // vida del jugador
-    [SerializeField] private float enemySpeed = 1;
-    [SerializeField] private Vector3 directionEnemy;
+    [SerializeField] private float enemyRate = 5;
     GameObject player;
-    //[SerializeField] private Vector3 size;
-    //public float enemySpeed;
-    //public float damage;
-    //[SerializeField] private Character character;
 
     // Start is called before the first frame update
     void Start()
     {
         //damage = 2;
         enemyName = "Drone 166";
-        enemySpeed = 0.1f;
+        //enemySpeed = 0.1f;
         player = GameObject.Find("Bubleship");
-        // vida CHAR = vida CHAR - daño ES - defensa CHAR
-        // character.health = character.health - damage - character.defense;
-        // character.health -= damage - character.defense;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.LookAt(player.transform); // el enemigo rota hacia el player
-        Movement(0.1f, new Vector3(0, 0.01f, 0));
-        
-        //Debug.Log("Te speed is: " + enemySpeed);
-        //transform.position += movement;
-        //transform.localScale += size;
-        //transform.position += enemySpeed * Time.deltaTime * transform.forward;
-
+        transform.LookAt(player.transform); // el enemigo rota (mira) hacia el player
+        GetComponent<Rigidbody>().velocity = transform.forward * enemyRate;
     }
 
-    // daño
-    public void enemyDamage()
+    private void OnCollisionEnter(Collision collision)
     {
-        enemyHealth = enemyHealth -2;
+        if (collision.collider.name == "Bubleship")
+            Debug.Log("El drone nos ha impactado");
+        if (collision.collider.tag == "Bullet")
+        {
+            Destroy(collision.collider.gameObject);
+            enemyHealth = enemyHealth - 20;
+            
+            if (enemyHealth == 0)
+            {
+                Destroy(gameObject);
+            }
+                
+        }
+
+
     }
-
-    // cura
-    public void enemyHeal()
-    {
-        enemyHealth = enemyHealth + 5;
-    }
-
-    
-    public void increaseEnemySpeed()
-    {
-        enemySpeed += 1;
-    }
-
-    public void decreaseEnemySpeed()
-    {
-        enemySpeed -= 1;
-    }
-
-    public void Movement(float speedEnemy, Vector3 directionEnemy)
-    {
-        //transform.position += transform.position * enemySpeed * Time.deltaTime;
-    }
-
-    private void LateUpdate()
-    {
-        //Debug.Log("The current altitude of the enemy is: " + transform.position.y);
-    }
-
-
 }
